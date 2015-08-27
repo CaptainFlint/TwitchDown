2bec3b340cedb0d32d58fb69cf86aff9e436d65a
Konstantin Vlasov <support@flint-inc.ru>
2015-08-28 00:33:30 +0300

    Added muted sections detection
    Since twitch only returns playlist with unmuted stream, but prohibits to download the "closed" segments, the playlist is downloaded locally and modified ("-muted" is added to the segment filename).
    Though the VOD JSON contains the list of muted sections, I discovered that the time markers in the playlist are wildly inaccurate and do not allow to determine the correct segment files to be muted. Therefore, mute check is performed by trying each segment file availability. If a segment file cannot be downloaded it is considered muted (and if the muted version also cannot be downloaded, an error is returned).
    Because this check is performed at startup and can take long time (several minutes), additional progress report was added.
    Known issue: for highlights, the playlist still reports the full time length, so the analyzing progress will stop before reaching the designated end (highlight is ended, but is shorter than the full video).

--- twitchdown.pl
+++ twitchdown.pl
@@ -1,15 +1,20 @@
 #!/usr/bin/perl
 
+# TODO: Add option to download only part of the video
+# TODO: Try to send playlist to ffmpeg's stdin while building it in runtime (therefore avoiding the startup analyzing stage)
+
 use strict;
 use warnings;
 
 use LWP::UserAgent;
+use JSON;
 
 if (scalar(@ARGV) < 2) {
 	print "Usage: $0 {URL|VideoID} {FileName}\n";
 	exit 1;
 }
 
+# Reading/parsing input arguments
 my ($vid, $file) = @ARGV;
 my $vid_type = 'v';
 if ($vid =~ m|http://www.twitch.tv/[^/]+/([^/])/(\d+)|) {
@@ -26,49 +31,179 @@ elsif ($vid !~ m/^\d+$/) {
 }
 my $vid_url = 'http://api.twitch.tv/api/videos/' . $vid_type . $vid;
 
-my $err = '';
-do {{
-	my $ua = LWP::UserAgent->new();
-	$ua->timeout(600);
-	my $res = $ua->get(
-		$vid_url,
+# Request overwriting the target file if necessary
+if (-f $file) {
+	print "File [$file] exists. Overwrite? (y/N) ";
+	my $repl = <STDIN>;
+	exit if ($repl !~ m/^y(es)?\s*$/i);
+}
+
+# Prepare downloader
+my $ua = LWP::UserAgent->new();
+$ua->timeout(600);
+
+sub http_request($;$) {
+	my ($url, $method) = @_;
+	if (!$method) {
+		$method = 'get';
+	}
+	my %headers = (
 		'User-Agent' => 'Opera/9.80 (Windows NT 6.1; Win64; x64) Presto/2.12.388 Version/12.17',
 		'Accept' => 'text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1',
 		'Accept-Language' => 'ru-RU,ru;q=0.9,en;q=0.8'
 	);
-	if (!$res->is_success) {
-		$err = "Failed to download JSON: " . $res->status_line;
-		last;
+	if ($method eq 'get') {
+		return $ua->get($url, %headers);
 	}
-	my $content = $res->decoded_content;
-	if (!$content) {
-		$err = "Failed to obtain JSON decoded contents: " . $res->status_line;
-		last;
-	}
-	if ($content !~ m/"preview"\s*:\s*"([^\"]+)"/) {
-		$err = "JSON does not contain preview URL.";
-		last;
-	}
-#	print $content . "\n";
-	my $m3u = $1;
-	my $hl = 0;
-	if ($content !~ m/"can_highlight"\s*:\s*true/) {
-		$hl = 1;
-	}
-	$m3u =~ s/static-cdn\.jtvnw\.net/vod\.ak\.hls\.ttvnw\.net/;
-	if ($hl) {
-		$m3u =~ s/thumb\/thumb.*\.jpg/chunked\/highlight-$vid.m3u8/;
+	if ($method eq 'head') {
+		return $ua->head($url, %headers);
 	}
 	else {
+		return undef;
+	}
+}
+
+sub format_time($) {
+	my ($sec) = @_;
+	return sprintf('%d:%02d:%02d', int($sec / 3600), int(($sec % 3600) / 60), int($sec % 60));
+}
+
+my $err = '';
+my $playlist_file = '';
+do {{
+	# Download the VOD JSON
+	my $res = http_request($vid_url);
+	if (!$res->is_success) {
+		$err = 'Failed to download JSON: ' . $res->status_line;
+		last;
+	}
+	my $json_txt = $res->decoded_content;
+	if (!$json_txt) {
+		$err = 'Failed to obtain JSON decoded contents: ' . $res->status_line;
+		last;
+	}
+	my $json = decode_json($json_txt);
+	if (!$json->{'preview'}) {
+		$err = 'JSON does not contain preview URL.';
+		last;
+	}
+
+	# Construct the playlist URL
+	my $m3u = $json->{'preview'};
+	$m3u =~ s/static-cdn\.jtvnw\.net/vod\.ak\.hls\.ttvnw\.net/;
+	if ($json->{'can_highlight'}) {
 		$m3u =~ s/thumb\/thumb.*\.jpg/chunked\/index-dvr.m3u8/;
 	}
-	if (-f $file) {
-		print "File \"$file\" exists. Overwrite? (y/N) ";
-		my $repl = <STDIN>;
-		last if ($repl !~ m/^y(es)?\s*$/i);
+	else {
+		$m3u =~ s/thumb\/thumb.*\.jpg/chunked\/highlight-$vid.m3u8/;
 	}
-	system('C:/Programs/ffmpeg/bin/ffmpeg.exe -y -i ' . $m3u . ' -c copy -bsf:a aac_adtstoasc "' . $file . '"');
+
+	# Download the playlist
+	$res = http_request($m3u);
+	if (!$res->is_success) {
+		$err = "Failed to download playlist $m3u:\n" . $res->status_line;
+		last;
+	}
+	my $playlist = $res->decoded_content;
+	if (!$playlist) {
+		$err = "Failed to obtain playlist decoded contents: " . $res->status_line;
+		last;
+	}
+
+	# Save the playlist, modifying it for:
+	# 1) being local;
+	# 2) compactification;
+	# 3) using correct URLs for muted parts.
+	my $playlist_fh;
+	$playlist_file = "C:/Users/CaptainFlint/AppData/Local/Temp/index-dvr-$vid.m3u8";
+	if (!open($playlist_fh, '>', $playlist_file)) {
+		$err = "Failed to open temp file [$playlist_file]:\n$!";
+		last;
+	}
+	binmode($playlist_fh);
+	my $oldh = select(STDOUT);
+	$| = 1;
+	select($oldh);
+	$m3u =~ s|/[^/]+$|/|;
+	my $dt;
+	my $dt_sum;
+	my $dt_sum_total;
+	my $current_ts = '';
+	my $current_ts_start;
+	my $current_ts_end;
+	my $is_muted = 0;
+	my $total_time;
+	my $progress_line;
+	my $dump_current_ts = sub() {
+		if ($current_ts) {
+			# Dump the previously collected segment
+			printf $playlist_fh "#EXTINF:%.3f,\n", $dt_sum;
+			print $playlist_fh "$m3u$current_ts" . ($is_muted ? '-muted' : '') . ".ts?start_offset=$current_ts_start&end_offset=$current_ts_end\n";
+			$dt_sum_total += $dt_sum;
+			print "\x08" x length($progress_line);
+			$progress_line = 'Analyzing video: ' . format_time($dt_sum_total) . '/' . format_time($total_time) . '...';
+			print $progress_line;
+			$current_ts = '';
+		}
+	};
+	for my $ln (split(m/\r?\n/, $playlist)) {
+		if ($ln =~ m/^\x23EXT-X-TWITCH-TOTAL-SECS\s*:\s*([.\d]+)/) {
+			$total_time = int($1);
+			$progress_line = 'Analyzing video: ' . format_time(0) . '/' . format_time($total_time) . '...';
+			print $progress_line;
+		}
+
+		if ($ln =~ m/^\x23EXTINF\s*:\s*([.\d]+),/) {
+			$dt = $1;
+		}
+		elsif ($ln =~ m/^(index-[^.]+)\.ts\?start_offset=(\d+)&end_offset=(\d+)/) {
+			if ($1 ne $current_ts) {
+				# New segment file
+				$dump_current_ts->();
+				($current_ts, $current_ts_start, $current_ts_end) = ($1, $2, $3);
+				$dt_sum = $dt;
+				$is_muted = 0;
+				$res = http_request($m3u . $ln, 'head');
+				if (!$res->is_success) {
+					$err = "Segment $ln is unvailable.\nUnmuted: " . $res->status_line;
+					$is_muted = 1;
+					$res = http_request("$m3u$current_ts-muted.ts?start_offset=$current_ts_start&end_offset=$current_ts_end");
+					if ($res->is_success) {
+						$err = '';
+					}
+					else {
+						$err .= "\nMuted: " . $res->status_line;
+						last;
+					}
+				}
+			}
+			else {
+				# Same segment file continued - merging
+				$current_ts_end = $3;
+				$dt_sum += $dt;
+			}
+		}
+		else {
+			$dump_current_ts->();
+			print $playlist_fh "$ln\n";
+		}
+	}
+	close($playlist_fh);
+	if ($err) {
+		print " Failed!\n";
+		last;
+	}
+	else {
+		print " Finished, starting download.\n";
+	}
+
+	# Finally, launch ffmpeg to do the rest of work
+	system('C:/Programs/ffmpeg/bin/ffmpeg.exe -y -i ' . $playlist_file . ' -c copy -bsf:a aac_adtstoasc "' . $file . '"');
 }} while (0);
+
+# Some cleanup
+unlink($playlist_file);
+
 if ($err) {
 	print STDERR "Error: $err\n";
 	exit 2;
