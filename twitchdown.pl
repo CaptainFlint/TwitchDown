325ce32b681c338fbc199818284485ee6ecdc976
Konstantin Vlasov <support@flint-inc.ru>
2018-01-30 00:41:22 +0300

    Numerous adaptations to Twitch changes and bugfixes.
    * Moved options to INI file.
    * Added support for twitch oauth.
    * Moved LWP::UserAgent initialization into thread to avoid thread-unsafety (did not help).
    * Cursor positioning logging.
    * Report muted segments in merged ranges instead of each one separately.
    * All warnings are duplicated into the INFO file (in case of batch downloads).
    * Various minor fixes and improvements.

--- twitchdown.pl
+++ twitchdown.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use LWP::UserAgent;
+use FindBin;
 use JSON;
 use File::Path 'remove_tree';
 use Term::ANSIColor;
@@ -13,25 +13,40 @@ if ($^O eq 'MSWin32') {
 	require Win32::Mutex;
 }
 
-# How many threads to use for downloading video segments
-my $NUM_THREADS = 12;
+my $options = {
+	# Directory for downloading segment files
+	'data_dir' => 'C:/Users/CaptainFlint/AppData/Local/Temp',
+	# Full path to ffmpeg executable
+	'ffmpeg_exe' => 'C:/Programs/ffmpeg/bin/ffmpeg.exe',
+	# Twitch authentication token
+	'twitch_auth' => '',
+	# How many threads to use for downloading video segments
+	'num_threads' => 12,
+	# Number of segment download retries
+	'num_retries' => 5,
+	# Whether to try mergning segments by file names (seems to not work lately)
+	'ts_merge' => 0,
+};
 
-# Number of segment download retries
-my $NUM_RETRIES = 5;
-
-my $TS_MERGE = 0;
-
-# Directory for downloading segment files
-my $data_dir = 'C:/Users/CaptainFlint/AppData/Local/Temp';
-
-# Full path to ffmpeg executable
-my $ffmpeg_exe = 'C:/Programs/ffmpeg/bin/ffmpeg.exe';
+my $ini_file = $FindBin::Bin . '/twitchdown.ini';
+my $ini_fh;
+if (open($ini_fh, '<', $ini_file)) {
+	while (<$ini_fh>) {
+		if (m/^([^\x23]\S*)\s*=\s*(.*)/) {
+			$options->{$1} = $2;
+		}
+	}
+}
 
 if (scalar(@ARGV) < 2) {
 	print <<EOF;
 Usage: $0 {URL|VideoID} {FileName} [OPTIONS]
 
 Options:
+  --token=AUTH       Twitch authentication token. To obtain one, go to 
+                     https://api.twitch.tv/kraken/oauth2/authorize/?response_type=token&client_id=ewvlchtxgqq88ru9gmfp1gmyt6h2b93&scope=user_read+user_subscriptions&redirect_uri=http://livestreamer.tanuki.se/en/develop/twitch_oauth.html
+                     and after you authorize you'll get redirected to a
+                     non-existent page; get the access_token value from the URL.
   --start=TIME       Download video starting from the specified timestamp.
   --end=TIME         Download video up to the specified timestamp.
   --len=TIME         Download video only the specified length of time.
@@ -76,6 +91,9 @@ for (@opts) {
 		}
 		$vod_time{$k} = $v;
 	}
+	elsif (m/^--token=(\S+)/) {
+		$options->{'twitch_auth'} = $1;
+	}
 	else {
 		print colored("WARNING: Unknown option '$_', skipping.\n", 'bold yellow');
 	}
@@ -117,13 +135,18 @@ if (-f $file) {
 	exit if ($repl !~ m/^y(es)?\s*$/i);
 }
 
-# Prepare downloader
-my $ua = LWP::UserAgent->new();
-$ua->timeout(600);
-$ua->ssl_opts(verify_hostname => 0);
+# Downloader
+my $ua;
 
 sub http_request($;$) {
 	my ($url, $method) = @_;
+	# Lazy downloader initialization
+	if (!$ua) {
+		require LWP::UserAgent;
+		$ua = LWP::UserAgent->new();
+		$ua->timeout(600);
+		$ua->ssl_opts(verify_hostname => 0);
+	}
 	if (!$method) {
 		$method = 'get';
 	}
@@ -172,21 +195,62 @@ sub is_skipped($$) {
 	}
 }
 
+use Fcntl qw/:flock :seek/;
+my $LOG_FILE = 'S:/twitch/virtualairshows/twitchdown-coords.log';
+unlink($LOG_FILE);
+# Append message to the log
+# Input parameters:
+#	$msg - text message to write
+# Return value:
+#	none
+sub LOG($) {
+	my ($msg) = @_;
+	my $log_fh;
+	if (open($log_fh, '>>', $LOG_FILE)) {
+		flock($log_fh, LOCK_EX);
+		seek($log_fh, 0, SEEK_END); # In the case if someone has written to the file in between open() and flock().
+		my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime();
+		my $date = sprintf('%02d.%02d.%04d %02d:%02d:%02d', $mday, ($mon + 1), ($year + 1900), $hour, $min, $sec);
+		print $log_fh "$date\t" . $msg . "\n";
+		flock($log_fh, LOCK_UN);
+		close($log_fh);
+	}
+}
+
 # Puts colored character at the specified position (0-based) of the progress bar.
 # Cursor originally is located at the end, and returned there after printing.
+
+# To debug:
+# perl K:\Programs\Perl\twitchdown\twitchdown.pl https://www.twitch.tv/virtualairshows/v/89921084 S:\twitch\virtualairshows\16.mp4 --token=XXXXXXX
+
+sub ensureCursorPos($$) {
+	my ($x, $y) = @_;
+	Win32::Console::ANSI::Cursor($x, $y);
+	for (my $i = 0; $i < 100; ++$i) {
+		my ($xpos, $ypos) = Win32::Console::ANSI::Cursor();
+		if (($xpos == $x) && ($ypos == $y)) {
+			last;
+		}
+		else {
+			++$i;
+			select(undef, undef, undef, 0.01);
+		}
+	}
+}
+
 my $put_char_mutex = Win32::Mutex->new();
+my ($xend, $yend);
 sub put_char($$$$) {
 	my ($char, $pos, $len, $color) = @_;
 	$put_char_mutex->wait();
 	my ($w, $h) = Win32::Console::ANSI::XYMax();
-	my ($x, $y) = Win32::Console::ANSI::Cursor();
 	# Position of the beginning of the progress bar (assume it always starts from newline)
-	my ($xstart, $ystart) = (1, $y - int($len / $w));
+	my ($xstart, $ystart) = (1, $yend - int($len / $w));
 	# Position of the character to insert
 	my ($xpos, $ypos) = ($xstart + ($pos % $w), $ystart + int($pos / $w));
-	Win32::Console::ANSI::Cursor($xpos, $ypos);
+	ensureCursorPos($xpos, $ypos);
 	print colored($char, $color);
-	Win32::Console::ANSI::Cursor($x, $y);
+	ensureCursorPos($xend, $yend);
 	$put_char_mutex->release();
 }
 
@@ -196,7 +260,7 @@ do {{
 	my @warnings = ();
 
 	# Request access token
-	my $token_url = 'https://api.twitch.tv/api/vods/' . $vid . '/access_token?as3=t&oauth_token=';
+	my $token_url = 'https://api.twitch.tv/api/vods/' . $vid . '/access_token?as3=t&oauth_token=' . $options->{'twitch_auth'};
 	my $res = http_request($token_url);
 	if (!$res->is_success) {
 		$err = 'Failed to download JSON: ' . $res->status_line;
@@ -261,8 +325,8 @@ do {{
 	# 2) compactification.
 	my @segment_urls = ();
 	my $playlist_fh;
-	$playlist_file = "$data_dir/index-dvr-$vid.m3u8";
-	mkdir("$data_dir/twitch-vod-$vid");
+	$playlist_file = $options->{'data_dir'} . "/index-dvr-$vid.m3u8";
+	mkdir($options->{'data_dir'} . "/twitch-vod-$vid");
 	if (!open($playlist_fh, '>', $playlist_file)) {
 		$err = "Failed to open temp file [$playlist_file]:\n$!";
 		last;
@@ -284,7 +348,12 @@ do {{
 		if ($current_ts) {
 			# Check if the previously collected segment is muted
 			if ($current_ts =~ m/-muted/) {
-				push @muted, format_time($dt_sum_total) . '-' . format_time($dt_sum_total + $dt_sum);
+				if ((scalar(@muted) > 0) && ($muted[scalar(@muted) - 1]->[1] == $dt_sum_total)) {
+					$muted[scalar(@muted) - 1]->[1] = $dt_sum_total + $dt_sum;
+				}
+				else {
+					push @muted, [$dt_sum_total, $dt_sum_total + $dt_sum];
+				}
 			}
 			# Dump the previously collected segment
 			my $segment_file = $current_ts . '.ts';
@@ -292,7 +361,7 @@ do {{
 			my $suffix = '';
 			if (defined($current_ts_start) && defined($current_ts_end)) {
 				push @segment_urls, "$m3u$segment_file?start_offset=$current_ts_start&end_offset=$current_ts_end";
-				if (!$TS_MERGE) {
+				if (!$options->{'ts_merge'}) {
 					$suffix = "-$current_ts_start-$current_ts_end.ts";
 				}
 			}
@@ -314,7 +383,7 @@ do {{
 			push @warnings, "Stream contains a gap of $1 seconds at " . format_time($dt_sum_total + $dt_sum) . ".";
 		}
 		elsif ($ln =~ m/^([^.]+)\.ts(?:\?start_offset=(\d+)&end_offset=(\d+))?$/) {
-			if (!$TS_MERGE || ($1 ne $current_ts)) {
+			if (!$options->{'ts_merge'} || ($1 ne $current_ts)) {
 				# New segment file
 				$dump_current_ts->();
 				if (!is_skipped($dt_sum_total + $dt_sum, $dt)) {
@@ -347,19 +416,26 @@ do {{
 	}
 	close($playlist_fh);
 	if (scalar(@muted) > 0) {
-		push @warnings, "Muted segments are present:\n" . join("\n", @muted);
+		push @warnings, "Muted segments are present:\n" . join("\n", map { format_time($_->[0]) . '-' . format_time($_->[1]) } @muted);
 	}
 	last if ($err);
 
 	print colored("!!! WARNING: $_\n", 'bold yellow') foreach (@warnings);
 	print "\n";
+	if (scalar(@warnings) > 0) {
+		my $fh;
+		open($fh, '>', $file . '.info') or die $!;
+		print $fh "!!! WARNING: $_\n" foreach (@warnings);
+		close($fh);
+	}
 
 	my $seg_num = scalar(@segment_urls);
-	my $seg_num_part = int($seg_num / $NUM_THREADS);
-	++$seg_num_part if ($seg_num % $NUM_THREADS != 0);
+	my $seg_num_part = int($seg_num / $options->{'num_threads'});
+	++$seg_num_part if ($seg_num % $options->{'num_threads'} != 0);
 	print "Downloading segments: $seg_num\n" . ('.' x $seg_num);
+	($xend, $yend) = Win32::Console::ANSI::Cursor();
 	my @children = ();
-	for (my $tid = 0; $tid < $NUM_THREADS; ++$tid) {
+	for (my $tid = 0; $tid < $options->{'num_threads'}; ++$tid) {
 		my $pid = fork();
 		if (!defined($pid)) {
 			$err = "Failed to fork: $!";
@@ -376,23 +452,33 @@ do {{
 				my $seg_url = $segment_urls[$i];
 				my $seg_file = ($seg_url =~ s|^.*/([^/?]+)(?:\?start_offset=(\d+)&end_offset=(\d+))?$|$1|r);
 				my ($seg_start, $seg_end) = ($2, $3);
-				for (my $j = 0; $j <= $NUM_RETRIES; ++$j) {
-					put_char(($j ? ($j + 1) : '?'), $i, $seg_num, 'bold yellow');
+				for (my $j = 0; $j <= $options->{'num_retries'}; ++$j) {
+					my $ch;
+					if ($j == 0) {
+						$ch = '?';
+					}
+					elsif (($j >= 1) && ($j <= 8)) {
+						$ch = ($j + 1);
+					}
+					else {
+						$ch = chr($j - 9 + 65);
+					}
+					put_char($ch, $i, $seg_num, 'bold yellow');
 					$res = http_request($seg_url);
 					if (!$res->is_success) {
-						if ($j == $NUM_RETRIES) {
+						if ($j == $options->{'num_retries'}) {
 							$err = "[$tid] Failed to download segment $seg_file: " . $res->status_line;
 							last;
 						}
 						else {
-#print colored("[$tid] Segment file No.$i '$seg_file' download failed, retrying (" . ($j + 2) . "/$NUM_RETRIES).\n", 'bold yellow');
+#print colored("[$tid] Segment file No.$i '$seg_file' download failed, retrying (" . ($j + 2) . "/" . $options->{'num_retries'} . ").\n", 'bold yellow');
 							next;
 						}
 					}
 #print "[$tid] Segment URL: $seg_url\nLength: " . $res->header('Content-Length') . "\n";
 					my $seg_fh;
-					my $suffix = ((!$TS_MERGE && defined($seg_start) && defined($seg_end)) ? "-$seg_start-$seg_end.ts" : '');
-					my $seg_file = "$data_dir/twitch-vod-$vid/$seg_file$suffix";
+					my $suffix = ((!$options->{'ts_merge'} && defined($seg_start) && defined($seg_end)) ? "-$seg_start-$seg_end.ts" : '');
+					my $seg_file = $options->{'data_dir'} . "/twitch-vod-$vid/$seg_file$suffix";
 #print "[$tid] Saving file $seg_file\n";
 					if (!open($seg_fh, '>', $seg_file)) {
 						$err = "[$tid] Failed to open segment file $seg_file:\n$!";
@@ -401,11 +487,18 @@ do {{
 					binmode($seg_fh);
 					print $seg_fh ${$res->content_ref};
 					close($seg_fh);
-					if (-s $seg_file == $res->header('Content-Length')) {
+					if ($res->header('Content-Length') && (-s $seg_file == $res->header('Content-Length'))) {
 						$success = 1;
 						last;
 					}
-#print colored("[$tid] Segment file No.$i '$seg_file' download failed, retrying (" . ($j + 2) . "/$NUM_RETRIES).\n", 'bold yellow');
+					else {
+						if ($j == $options->{'num_retries'}) {
+							$err = "[$tid] Failed to save segment $seg_file: " . $!;
+							last;
+						}
+						next;
+					}
+#print colored("[$tid] Segment file No.$i '$seg_file' download failed, retrying (" . ($j + 2) . "/" . $options->{'num_retries'} . ").\n", 'bold yellow');
 				}
 				if ($err) {
 					put_char('X', $i, $seg_num, 'bold red');
@@ -415,11 +508,11 @@ do {{
 #print colored("[$tid] Segment file No.$i '$seg_file' done.\n", 'bold green');
 			}
 			if ($err) {
-#				print STDERR colored("$err\n", 'bold red');
+				print STDERR colored("$err\n", 'bold red');
 				exit(1);
 			}
 			else {
-#				print colored("[$tid] Finished.\n", 'bold green');
+#print colored("[$tid] Finished.\n", 'bold green');
 				exit(0);
 			}
 		}
@@ -434,18 +527,25 @@ do {{
 		last;
 	}
 	while (($res = wait()) != -1) {
-#		print "wait returned $res, thread's exit code: " . ($? >> 8) . "\n";
-		if ($?) {
+		my $retcode = ($? >> 8);
+#print "wait returned $res, thread's exit code: $retcode\n";
+		if ($retcode) {
+#print "retcode\n";
 			$err = "Some of the segments could not be downloaded, aborting.\n";
-			kill 'KILL', @children;
+#print "err\n";
+			kill 'TERM', @children;
+#print "kill\n";
 			last;
+		}
+		else {
+#print "not retcode\n";
 		}
 	}
 	print "\n";
 	last if ($err);
 
 	# Finally, launch ffmpeg to do the rest of work
-	if (system($ffmpeg_exe . ' -y -i ' . $playlist_file . ' -c copy -bsf:a aac_adtstoasc "' . $file . '"') != 0) {
+	if (system($options->{'ffmpeg_exe'} . ' -y -i ' . $playlist_file . ' -c copy -bsf:a aac_adtstoasc "' . $file . '"') != 0) {
 		print STDERR colored("Error: Failed to build MP4. Segment files are not removed.\n", 'bold red');
 		exit 2;
 	}
@@ -453,7 +553,7 @@ do {{
 
 # Some cleanup
 unlink($playlist_file);
-remove_tree("$data_dir/twitch-vod-$vid");
+remove_tree($options->{'data_dir'} . "/twitch-vod-$vid");
 # FIXME: Clean up segment files
 
 if ($err) {
