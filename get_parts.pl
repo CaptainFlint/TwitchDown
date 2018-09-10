f48a82dc0c52bd8613464364e1d8fd7703ac6d00
Konstantin Vlasov <support@flint-inc.ru>
2018-09-11 01:11:51 +0300

    Batch downloader improvements
    * Added optional file part identifier suffix to override the normal increment.
    * Start time can be omitted now.
    * Added overriding for video quality and download directory.

--- get_parts.pl
+++ get_parts.pl
@@ -3,39 +3,45 @@
 use strict;
 use warnings;
 
-my $url = 'https://www.twitch.tv/squirrel/v/81915754';
-my $name = '160805-fernbus+jalopy';
+my $url = 'https://www.twitch.tv/videos/255822477';
+my $name = '180429-snt';
+my $quality = '';
+#my $quality = '720p60';
+my $force_path = "E:\\DiskE\\Temp\\!twitch";
 my @parts = split(m/\s+/s, <<DATA);
-	1.10/18.11/3/6.40-1.18/18.19/4/2.50
-	1.25/18.26/1/7.59-1.47
-	1.52/9/6.12
-	2.13/19.14/12/1.40-4.20/21.21/3/7.00
-	4.46/21.47-5.03/22.04/4/6.10
+	1.18-2.25
+	f1:3.38
 DATA
+my $idx = 1;
 
 sub shift_time($$) {
 	my ($tm, $adj) = @_;
 	my ($hr, $min) = split(m/\./, $tm);
-	$min += $adj;
-	if ($min < 0) {
-		$min += 60;
-		--$hr;
-	}
-	elsif ($min >= 60) {
-		$min -= 60;
-		++$hr;
+	if ($tm) {
+		$min += $adj;
+		if ($min < 0) {
+			$min += 60;
+			--$hr;
+		}
+		elsif ($min >= 60) {
+			$min -= 60;
+			++$hr;
+		}
 	}
 	return sprintf("%d:%02d", $hr, $min);
 }
 
-my $idx = 1;
 foreach my $part (@parts) {
 	next if ($part eq '');
+	if (index($part, ':') >= 0) {
+		($idx, $part) = split(m/:/, $part);
+	}
 	my ($start, $end) = split(m/-/, $part);
 	$end = $start if (!defined($end));
 	$start =~ s/\/.*//g;
 	$end =~ s/\/.*//g;
-	my $cmdline = "perl K:\\Programs\\Perl\\twitchdown\\twitchdown.pl $url E:\\DiskE\\Temp\\!twitch\\${name}_$idx.mp4 --start=" . shift_time($start, -3) . ($end ? (" --end=" . shift_time($end, +3)) : '');
+	my $target = ($quality ? (($force_path || "N:\\unsorted\\_twitch") . "\\${name}_$idx-$quality.mp4") : "E:\\DiskE\\Temp\\!twitch\\${name}_$idx.mp4");
+	my $cmdline = "perl K:\\Programs\\Perl\\twitchdown\\twitchdown.pl $url $target" . ($quality ? ' --quality=' . $quality : '') . ($start ? (" --start=" . shift_time($start, -3)) : '') . ($end ? (" --end=" . shift_time($end, +3)) : '');
 	print "$cmdline\n";
 	system($cmdline);
 	print "\n\n";
